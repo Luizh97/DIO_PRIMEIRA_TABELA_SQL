192d1d79e6f9687f4be33218d3c0057141ddab99
Luizh97 <luizhenriquee97@gmail.com>
2022-03-03 16:15:06 -0300

    Adição da primeira tabela do curso de mysql

---
+++ dio.sql
@@ -0,0 +1,10 @@
+CREATE TABLE pessoas (
+id INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+nome VARCHAR(30) NOT NULL,
+nascimento DATE
+
+)
+
+INSERT INTO pessoas (nome, nascimento) VALUES ('Luiz Henrique', '1997-02-25')
+INSERT INTO pessoas (nome, nascimento) VALUES ('Clara', '1998-08-20')
+INSERT INTO pessoas (nome, nascimento) VALUES ('Renilda', '1966-11-01')
